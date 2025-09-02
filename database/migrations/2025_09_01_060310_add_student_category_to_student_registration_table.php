f2ba374f0f5906b30aea1de4df67dac8f3d590f4
Eliab <35658748+eliabsiji@users.noreply.github.com>
2025-09-02 04:13:02 +0100

    Update 2025_09_01_060310_add_student_category_to_student_registration_table.php

--- database/migrations/2025_09_01_060310_add_student_category_to_student_registration_table.php
+++ database/migrations/2025_09_01_060310_add_student_category_to_student_registration_table.php
@@ -1,22 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\Schemaation;
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprintprint;
-use Illuminate\Database\Migrations\Migrationchema;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;;
 
      return new class extends Migration
      {
          public function up(): void
          {
-             Schema::table('studentRegistration', function (Blueprint $table) {
+             Schema::table('', function (Blueprint $table) {
                  $table->string('student_category', 50)->nullable()->after('statusId');
              });
          }
 
          public function down(): void
          {
-             Schema::table('studentRegistration', function (Blueprint $table) {
+             Schema::table('', function (Blueprint $table) {
                  $table->dropColumn('student_category');
              });
          }
