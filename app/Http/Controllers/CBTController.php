751403e24ecfa48ed908b6178f85ac8f57dfce04
Eliab <35658748+eliabsiji@users.noreply.github.com>
2026-01-29 11:42:29 +0100

    Update CBTController.php

--- app/Http/Controllers/CBTController.php
+++ app/Http/Controllers/CBTController.php
@@ -56,7 +56,7 @@ public function index(Request $request)
     $sessionObj = $studentClassData ? (object) ['id' => $studentClassData->session_id, 'session'   => $studentClassData->session_name] : null;
 
     $terms    = Schoolterm::orderBy('id', 'desc')->get(['id', 'term']);
-    $sessions = Schoolsession::orderBy('id', 'desc')->get(['id', 'session', 'status']);
+    $sessions = Schoolsession::orderBy('id', 'desc')->get(['id', 'session']);
 
     $selectedTermId    = $request->query('term');
     $selectedSessionId = $request->query('session');
