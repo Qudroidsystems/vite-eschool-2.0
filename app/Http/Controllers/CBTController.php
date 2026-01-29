3f2a6f4a58379612b6f16804bb28e70329ad9b1d
Eliab <35658748+eliabsiji@users.noreply.github.com>
2026-01-29 09:39:51 +0100

    Update CBTController.php

--- app/Http/Controllers/CBTController.php
+++ app/Http/Controllers/CBTController.php
@@ -97,7 +97,7 @@ class CBTController extends Controller
                 ->whereIn('subject_id', $registeredSubjects)
                 ->where('schoolclass_id', $studentClassData->class_id)
                 ->where('termid', 2)
-                ->where('session', 2)
+                ->where('session', $studentClassData->session_id)
                 ->select('id', 'title', 'subject_id', 'description', 'duration', 'start_time', 'end_time')
                 ->paginate(15);
 
